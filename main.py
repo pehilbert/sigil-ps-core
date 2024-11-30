73c35f1099fbcec3e804788ebb4c024269eb978c
Peter Hilbert <pehilbert03@gmail.com>
2024-11-29 17:08:43 -0700

    Basic setup

---
+++ main.py
@@ -0,0 +1,44 @@
+import flask
+import tiamat
+
+from flask_cors import CORS
+
+# Create the Flask app
+app = flask.Flask(__name__)
+CORS(app)
+
+conversations = {}
+
+# Define a route
+@app.route('/api/prompt', methods=['POST'])
+def get_widget():
+    # Extract the prompt from the request
+    data = flask.request.get_json()
+    conversation_id = data.get('id')
+    message = data.get('message')
+
+    if not message or not conversation_id:
+        return flask.jsonify({'message': 'Some required data is missing'}), 400
+
+    if conversation_id in conversations:
+        chat = conversations[conversation_id]
+        print(f"Existing conversation detected, id={conversation_id}")
+        print(f"Last response: {chat.last_response}")
+    else:
+        chat = tiamat.Tiamat()
+        conversations[conversation_id] = chat
+        print(f"New conversation created, id={conversation_id}")
+
+    print(f" User's message: {message}")
+
+    response = chat(message)
+
+    print(f"       Response: {response}")
+    print(f"Updated context: {chat.context}")
+
+    # Return the widget as JSON
+    return flask.jsonify({'response': response})
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ tiamat.py
@@ -0,0 +1,37 @@
+import dspy
+import sys
+from dotenv import load_dotenv
+
+load_dotenv()
+
+verbose = len(sys.argv) > 1 and (sys.argv[1] == '-v' or sys.argv[1] == '--verbose')
+
+gpt = dspy.LM('openai/gpt-4o-mini')
+dspy.settings.configure(lm=gpt)
+
+class Tiamat(dspy.Module):
+	def __init__(self):
+		self.context = ""
+		self.last_response = ""
+		self.answer_question = dspy.Predict(AnswerQuestion)
+
+	def forward(self, message):
+		output = self.answer_question(context=self.context, last_response=self.last_response, student_message=message)
+		self.context = output.new_context
+		self.last_response = output.answer
+		return output.answer
+
+class AnswerQuestion(dspy.Signature):
+	"""
+	You are a computer science tutor for novice computer science students. Only provide answers/information 
+	that is directly asked for by the student, and when doing so, do not provide direct source code answers.
+	Try to respond with guiding questions whenever possible, and feel free to ask the student for any info
+	that would be useful for you in helping them.
+	"""
+
+	context = dspy.InputField(desc="Brief description of learned info about the student/conversation (what they're working on, skill level, etc.)")
+	last_response = dspy.InputField(desc="The last thing you said to the student, provides more context")
+	student_message = dspy.InputField(desc="Could be a question, their code, their problem, etc.")
+
+	answer = dspy.OutputField(desc="Concise response to student's message (no source code answers)")
+	new_context = dspy.OutputField(desc="Add to and/or update current context based on new message and/or answer")
