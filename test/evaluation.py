97b3fe950ded403a4631c3c3fc403f89e6438873
dja322 <dja322@nau.edu>
2025-04-02 15:30:49 -0700

    Runs evaluation for a given dataset given an array of metrics

--- test/evaluation.py
+++ test/evaluation.py
@@ -32,11 +32,23 @@ Output json file schema
         {
             "question": "",
             "code": "",
-            "answer": "",
-            "{Metric name}": 0.0
-        }
+            "answer": "",    
+            "metrics": [
+                {
+                    "metric": Some metric,
+                    "metric_score": 5
+                },
+                {
+                    "metric": "Some Metric",
+                    "metric_score": 5
+                }
+            ]
+        },
     ],
-    "overall_metric_score": 0.0
+    "overall_metric_score": {
+        "Tutor Similarity": Some score,
+        "Example Metric": Some score
+    }
 }
 '''
 
@@ -53,18 +65,21 @@ def main():
     file.close()
 
     #load data from file
-    data_filename = data_and_metric_json["data_file_path"]
+    data_filename = data_and_metric_json["datasets"]
     file = open(data_filename)
     json_data = json.loads(file.read())
     file.close()
     
     #load metric from file
-    metric_filenames = data_and_metric_json["metric_file_path"]
-    metric = json_to_metric_from_file(metric_filenames)
+    metric_filenames = data_and_metric_json["metrics"]
+    # metric = json_to_metric_from_file(metric_filenames)
+    metrics = [json_to_metric_from_file(metric_file) for metric_file in metric_filenames]
     output_dict = {
         "data": [],
         "overall_metric_score": 0
     }
+
+    metric_totals = {metric["name"]: 0 for metric in metrics}
 
     # loop through dataset
     for data in json_data["data"]:
@@ -74,20 +89,30 @@ def main():
                 "question": data["input"],
                 "code": data["code"],
                 "answer": data["actual_output"],
-                metric["name"] : 0
+                "metrics" : []
             }
-        print(f"metric: {metric}")
-        # Get the metric score for the current data
-        score = metric["evaluate"](data)
-        temp_dict[metric["name"]] = score
-        output_dict["overall_metric_score"] += score
+        
+        # Loop through each metric and evaluate the data
+        for metric in metrics:
+            print(f"metric: {metric}")
+            # Get the metric score for the current data
+            score = metric["evaluate"](data)
+            metric_totals[metric["name"]] += score
+            output_dict["overall_metric_score"] += score
+
+            temp_dict["metrics"].append(
+                    {
+                        "metric": metric["name"],
+                        "metric_score": score
+                    }
+                )
 
         # Add the metric score to the output dictionary
         output_dict["data"].append(temp_dict)
             
 
     # Log overall score of the evaluation of metrics
-    output_dict["overall_metric_score"] = output_dict["overall_metric_score"] / len(json_data["data"])
+    output_dict["overall_metric_score"] = {metric["name"]: metric_totals[metric["name"]] / len(json_data['data']) for metric in metrics}
 
     
     #loads second file and writes json log object
